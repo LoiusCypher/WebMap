e116e2fd1fced3635c324c481aa50d5a8faf8060
Helmut <helmut@knirsch.de>
2025-11-23 01:43:48 +0100

    Test cron get it executable

--- nmap/test_cron.py
+++ nmap/test_cron.py
@@ -1,16 +1,12 @@
 from django.test import TestCase
 import cron
 
-from django.conf import settings
-import os, re, json, time
-import subprocess, shutil, shlex
-
 class CronTestCase(TestCase):
 	def setUp(self):
 		self.sched = json.loads({
-			'number': 0
-			'params': {
-				'filename': 'testfile.xml'
+			"number": 0,
+			"params": {
+				"filename": "testfile.xml"
 			}
 		})
 	def test_cron_generate_tmp_file_name():
