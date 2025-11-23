d337e918e24278d74cc9d20520cb81339b01a9c6
Helmut <helmut@knirsch.de>
2025-11-23 02:49:58 +0100

    Test cron second test

--- nmap/test_cron.py
+++ nmap/test_cron.py
@@ -1,19 +1,20 @@
 from django.test import TestCase
 import cron
-#import json
+from django.conf import settings
 
 class CronTestCase(TestCase):
 	def setUp(self):
+		self.params = ['-sT', '-A', '-T4']
 		self.sched = {
 			'number': 3,
 			'params': {
 				'filename': 'testfile.xml',
-				'params': '-sT -A -T4',
+				'params': join(self.params),
 				'target': '192.168.2.112/24'
 			}
 		}
 	def test_cron_generate_tmp_file_name(self):
 		self.assertEqual(cron.cron_gen_tmp_file_name(self.sched),'/tmp/3_testfile.xml.active')
 	def test_cron_gen_nmap_list(self):
-		self.assertEqual(cron_gen_nmap_list(self.sched),'/tmp/3_testfile.xml.active')
+		self.assertEqual(cron.cron_gen_nmap_list(self.sched), ['/usr/local/bin/nmap']+self.params+['--script='+os.path.join(os.path.dirname(os.path.realpath(__file__)),'nse',)+'/', '-oX', cron.cron_gen_tmp_file_name(self.sched), self.sched['params']['target']])
 
