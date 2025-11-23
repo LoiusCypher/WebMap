90530fd4c26e1548af6f8dcd9c77dba88e30b9ad
Helmut <helmut@knirsch.de>
2025-11-23 01:55:26 +0100

    Test cron get json cceptable

--- nmap/test_cron.py
+++ nmap/test_cron.py
@@ -6,9 +6,10 @@ class CronTestCase(TestCase):
 	def setUp(self):
 		self.sched = json.loads({
 			"number": 0,
-			"params": {
-				"filename": "testfile.xml"
-			}
+			"params": ""
+#			"params": {
+#				"filename": "testfile.xml"
+#			}
 		})
 	def test_cron_generate_tmp_file_name():
 		self.assertEqual(cron.cron_gen_tmp_file_name(self.sched),"o")
