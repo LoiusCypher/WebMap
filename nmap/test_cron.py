9880da332bd7b1ba85be2e61657cbda548365982
Helmut <helmut@knirsch.de>
2025-11-23 01:29:46 +0100

    Test cron test failing

---
+++ nmap/test_cron.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+import cron
+
+from django.conf import settings
+import os, re, json, time
+import subprocess, shutil, shlex
+
+Class CronTestCase(TestCase):
+	def setUp(self):
+		self.sched = json.loads({
+			'number': 0
+			'params': {
+				'filename': 'testfile.xml'
+			}
+		})
+	def test_cron_generate_tmp_file_name():
+		self.assertEqual(cron.cron_gen_tmp_file_name(self.sched),"o")
+
