e288b47f4d0837863184c4ff68afcdb225946d88
Helmut <helmut@knirsch.de>
2025-11-23 12:48:41 +0100

    Fix test

--- nmap/test_cron.py
+++ nmap/test_cron.py
@@ -25,10 +25,10 @@ class CronTestCase(TestCase):
 		self.assertEqual(cron.genFinishedScanFileName(self.sched),'webmapsched_763592814.9651988_testfile.xml')
 
 	def test_cron_genScanCmd(self):
-		self.assertEqual(cron.genScanCmd(self.sched), [ '/usr/bin/nmap' ]+self.sched['params']['params']+['--script='+os.path.join(os.path.dirname(os.path.realpath(__file__)),'nse',)+'/', '-oX', cron.genActiveScanFilePath(self.sched), self.sched['params']['target']])
+		self.assertEqual(cron.genScanCmd(self.sched), [ '/usr/bin/nmap' ]+[self.sched['params']['params']]+['--script='+os.path.join(os.path.dirname(os.path.realpath(__file__)),'nse',)+'/', '-oX', cron.genActiveScanFilePath(self.sched), self.sched['params']['target']])
 
 	def test_cron_runScan_successs(self):
-		xpected_string_start='[DONE] Starting Nmap 7.94SVN ( https://nmap.org ) at 2025-11-23 08:46 UTC\n - Nmap done: 256 IP addresses (0 hosts up) scanned in 51.68 seconds'
+		expected_string_start='[DONE] Starting Nmap 7.94SVN ( https://nmap.org ) at 2025-11-23 08:46 UTC\n - Nmap done: 256 IP addresses (0 hosts up) scanned in 51.68 seconds'
 		retVal, active_scan_file_path, stdout, stderr = cron.runScan(self.sched)
 		self.assertEqual(retVal,0)
 		self.assertEqual(active_scan_file_path,cron.genActiveScanFilePath(self.sched))
