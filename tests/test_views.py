6f4f8b141da106e22aceecff2178558afc641755
Helmut <helmut@knirsch.de>
2025-11-25 21:34:00 +0100

    Forgot to include first views tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-import nmapreport.views
+import django.nmapreport.views
 from django.conf import settings
+import os
 
 
 class ViewsTestCase(TestCase):
@@ -11,6 +12,6 @@ class ViewsTestCase(TestCase):
 		self.assertEqual(views.getCveOut(self.cvejson), '/home/runner/work/WebMap/WebMap/nmapdashboard/nmapreport/nmap/nse')
 
 	def test_cron_getCveOut_new(self):
-		cve_test = cve.loadScan(os.path.join(cdir, '.testfiles/new_cve.xml'))
+		cve_test = cve.loadScan(os.path.join(views.cdir, '.testfiles/new_cve.xml'))
 		self.assertEqual(views.getCveOut(cve_test), '/home/runner/work/WebMap/WebMap/nmapdashboard/nmapreport/nmap/nse')
 
