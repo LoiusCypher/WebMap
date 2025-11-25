e42e88f5048e5537b3c4fb3a4247c530218257c3
Helmut <helmut@knirsch.de>
2025-11-25 17:49:16 +0100

    Full Test flake W191

--- tests/test_cron.py
+++ tests/test_cron.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 import nmapreport.nmap.cron as cron
 import copy
 
+
 class CronTestCase(TestCase):
 	def setUp(self):
 		self.scan_options = ['-sT', '-A', '-T4']
@@ -18,16 +19,17 @@ class CronTestCase(TestCase):
 		self.fail_sched['params']['params'] = '-xX'
 
 	def test_cron_nse_path(self):
-		self.assertEqual(cron.nsePath(),'/home/runner/work/WebMap/WebMap/nmapdashboard/nmapreport/nmap/nse')
+		self.assertEqual(cron.nsePath(), '/home/runner/work/WebMap/WebMap/nmapdashboard/nmapreport/nmap/nse')
 
 	def test_cron_generate_active_scan_file_path(self):
-		self.assertEqual(cron.genActiveScanFilePath(self.sched),'/tmp/3_testfile.xml.active')
+		self.assertEqual(cron.genActiveScanFilePath(self.sched), '/tmp/3_testfile.xml.active')
 
 	def test_cron_generate_finished_scan_file_name(self):
-		self.assertEqual(cron.genFinishedScanFileName(self.sched),'webmapsched_763592814.9651988_testfile.xml')
+		self.assertEqual(cron.genFinishedScanFileName(self.sched), 'webmapsched_763592814.9651988_testfile.xml')
 
 	def test_cron_genScanCmd(self):
-		self.assertEqual(cron.genScanCmd(self.sched), [ '/usr/bin/nmap' ]+self.scan_options+['--script='+cron.nsePath()+'/', '-oX', cron.genActiveScanFilePath(self.sched), self.sched['params']['target']])
+		self.assertEqual(cron.genScanCmd(self.sched), [ '/usr/bin/nmap' ]+self.scan_options+['--script='+cron.nsePath()+'/',\
+				'-oX', cron.genActiveScanFilePath(self.sched), self.sched['params']['target']])
 
 	def test_cron_runScan_successs(self):
 		expected_strings=[
