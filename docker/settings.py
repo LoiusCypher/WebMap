dc6933989f6ee52dc2af6bf0640da8f5a6ab034c
Helmut <helmut@knirsch.de>
2025-11-27 21:36:14 +0100

    Handle Scans and ScanJobs through models

--- docker/settings.py
+++ docker/settings.py
@@ -29,7 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nmapreport.apps.NmapreportConfig',
+    'nmapreport',
+    'nmapreport.models',
+    'nmapreport.apps.nmapreportconfig',
 ]
 
 MIDDLEWARE = [
