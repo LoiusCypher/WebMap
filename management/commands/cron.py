8049defcb0e5c0c1852e4f96f7606856d5dd088a
Helmut <helmut@knirsch.de>
2025-11-27 17:07:05 +0100

    Start with notes to move to DB models

--- management/commands/cron.py
+++ management/commands/cron.py
@@ -56,8 +56,8 @@ class Command(BaseCommand):
 
         self.stdout.write(str(ScanJob.objects.all().count()))
         for sched in ScanJob.objects.all():
-            stdout.write(str(sched.date_last_execution))
-            stdout.write(str(sched.execution_interval_numer))
+            sched.stdout.write(str(sched.date_last_execution))
+            sched.stdout.write(str(sched.execution_interval_numer))
             nextrun = sched.date_last_execution + gethours(sched.execution_interval_numer)
             print("[RUN]   scan:" + sched.name + " id:" + str(sched.id) + " (nextrun:" + str(nextrun) + " / now:" + str(datetime.now()) + ")")
             if nextrun <= datetime.now():
@@ -66,20 +66,20 @@ class Command(BaseCommand):
                 self.stdout.write(str(sched.execution_counter))
                 sched.Save()
 
-                    # errorCode, nmap_active_scan_out, stdout, stderr = runScan(sched)
-                    # print('[DONE] ' + stderr + stdout)
-                    # if errorCode != 0:
-                        # os.remove(nmap_active_scan_out)
-                    # else:
-                        # time.sleep(5)
-                        # nmap_out_file = genFinishedScanFileName(sched)
-                        # shutil.move(nmap_active_scan_out, '/opt/xml/' + nmap_out_file)
-                        # nmapout = os.popen('python3 ' + cdir + '/cve.py webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename'] + '').readlines()
-                        # print(nmapout)
-                    # time.sleep(10)
-                # else:
-                    # print("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
-                # nextsched = min(nextsched, nextrun)
+            #        # errorCode, nmap_active_scan_out, stdout, stderr = runScan(sched)
+            #        # print('[DONE] ' + stderr + stdout)
+            #        # if errorCode != 0:
+            #            # os.remove(nmap_active_scan_out)
+            #        # else:
+            #            # time.sleep(5)
+            #            # nmap_out_file = genFinishedScanFileName(sched)
+            #            # shutil.move(nmap_active_scan_out, '/opt/xml/' + nmap_out_file)
+            #            # nmapout = os.popen('python3 ' + cdir + '/cve.py webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename'] + '').readlines()
+            #            # print(nmapout)
+            #        # time.sleep(10)
+            #    # else:
+            #        # print("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
+            #    # nextsched = min(nextsched, nextrun)
             # print("[DEBUG] nextsched:" + str(nextsched - time.time()))
 
 
