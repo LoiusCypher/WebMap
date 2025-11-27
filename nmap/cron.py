454ed5c6b84a8e767294b528b82fc1db148034a6
Helmut <helmut@knirsch.de>
2025-11-28 00:49:47 +0100

    Hunting model issue

--- nmap/cron.py
+++ nmap/cron.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
-# from nmapreport.models import Question as Poll
+# from nmapreport.models import Scan, ScanJob
+import nmapreport.models 
+from datetime import datetime
 import os
 import re
 import json
@@ -25,25 +27,29 @@ def gethours(f):
 	}[f]
 
 
-def genActiveScanFilePath(sched):
-	return '/tmp/' + str(sched['number']) + '_' + sched['params']['filename'] + '.active'
-
-
 def genFinishedScanFileName(sched):
 	return 'webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename']
 
 
-def genScanCmd(sched):
-	return [shutil.which('nmap')] + shlex.split(sched['params']['params']) + ['--script=' + nsePath() + '/',
-		'-oX', genActiveScanFilePath(sched), sched['params']['target']]
+def genActiveScanFilePath(name, number):
+    return '/tmp/' + str(number) + '_' + name + '.active'
 
 
-def runScan(sched):
-	nmap_active_scan_out = genActiveScanFilePath(sched)
-	nmapprocess = subprocess.Popen(genScanCmd(sched), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-	stdout, stderr = nmapprocess.communicate()
-	print('[DONE] ' + stderr + stdout)
-	return nmapprocess.returncode, nmap_active_scan_out, stderr, stdout
+def genScanCmd(name, params, target, number):
+    return [shutil.which('nmap')] \
+            + shlex.split('params') \
+            + ['--script=' + nsePath() + '/', '-oX', genActiveScanFilePath(name, number), target]
+
+
+def runScan(scan):
+    nmap_active_scan_out = genActiveScanFilePath(scan.name, scan.execution_counter)
+    nmapprocess = subprocess.Popen(genScanCmd(scan.name, scan.params, scan.target, scan.execution_counter),
+                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    scan.scan_end = datetime.now()
+    scan.save()
+    stdout, stderr = nmapprocess.communicate()
+    print('[DONE] ' + stderr + stdout)
+    return nmapprocess.returncode, nmap_active_scan_out, stderr, stdout
 
 
 def cron():
