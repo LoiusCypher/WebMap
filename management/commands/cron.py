2bcfe30d193b1dcd635480088605bbc8a11f1c57
Helmut <helmut@knirsch.de>
2025-11-27 11:36:45 +0100

    Start with notes to move to DB models

--- management/commands/cron.py
+++ management/commands/cron.py
@@ -47,55 +47,58 @@ def runScan(sched):
 	return nmapprocess.returncode, nmap_active_scan_out, stderr, stdout
 
 
-def cronMe(self):
-
-    schedfiles = os.listdir('/opt/schedule/')
-
-    self.stdout.write(ScanJob.objects.count())
-    nextsched = time.time() + gethours('1m')
-    for sched in ScanJob.objects.all():
-        nextrun = sched.date_last_executio + gethours(sched.execution_interval_nume)
-        self.stdout.write(nextrun)
-    for i in schedfiles:
-        if re.search(r'^[a-f0-9]{32,32}\.json$', i.strip()) is not None:
-            try:
-                sched = json.loads(open('/opt/schedule/' + i, "r").read())
-            except ValueError as e:
-                print("Error in json content of ", i, ': ', e)
-                continue
-            except IOError as e:
-                print("Error while rading  ", i, ': ', e)
-                continue
-
-            nextrun = (sched['lastrun'] + gethours(sched['params']['frequency']))
-            if nextrun <= time.time():
-                sched['number'] = (sched['number'] + 1)
-                print("[RUN]   scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
-
-                # first make sure to write down current run
-                sched['lastrun'] = time.time()
-                f = open('/opt/schedule/' + i, "w")
-                f.write(json.dumps(sched, indent=4))
-                nextrun = sched['lastrun'] + gethours(sched['params']['frequency'])
-
-                errorCode, nmap_active_scan_out, stdout, stderr = runScan(sched)
-                print('[DONE] ' + stderr + stdout)
-                if errorCode != 0:
-                    os.remove(nmap_active_scan_out)
-                else:
-                    time.sleep(5)
-                    nmap_out_file = genFinishedScanFileName(sched)
-                    shutil.move(nmap_active_scan_out, '/opt/xml/' + nmap_out_file)
-                    nmapout = os.popen('python3 ' + cdir + '/cve.py webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename'] + '').readlines()
-                    print(nmapout)
-                time.sleep(10)
-            else:
-                print("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
-            nextsched = min(nextsched, nextrun)
-        print("[DEBUG] nextsched:" + str(nextsched - time.time()))
-
-
 class Command(BaseCommand):
+
+    def cronMe(self):
+
+        schedfiles = os.listdir('/opt/schedule/')
+
+        self.stdout.write(ScanJob.objects.count())
+        nextsched = time.time() + gethours('1m')
+        for sched in ScanJob.objects.all():
+            nextrun = sched.date_last_executio + gethours(sched.execution_interval_nume)
+            self.stdout.write(nextrun)
+            sched.execution_counter += 1
+            self.stdout.write(sched.execution_counter)
+            sched.Save()
+        for i in schedfiles:
+            if re.search(r'^[a-f0-9]{32,32}\.json$', i.strip()) is not None:
+                try:
+                    sched = json.loads(open('/opt/schedule/' + i, "r").read())
+                except ValueError as e:
+                    print("Error in json content of ", i, ': ', e)
+                    continue
+                except IOError as e:
+                    print("Error while rading  ", i, ': ', e)
+                    continue
+
+                nextrun = (sched['lastrun'] + gethours(sched['params']['frequency']))
+                if nextrun <= time.time():
+                    sched['number'] = (sched['number'] + 1)
+                    print("[RUN]   scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
+
+                    # first make sure to write down current run
+                    sched['lastrun'] = time.time()
+                    f = open('/opt/schedule/' + i, "w")
+                    f.write(json.dumps(sched, indent=4))
+                    nextrun = sched['lastrun'] + gethours(sched['params']['frequency'])
+
+                    errorCode, nmap_active_scan_out, stdout, stderr = runScan(sched)
+                    print('[DONE] ' + stderr + stdout)
+                    if errorCode != 0:
+                        os.remove(nmap_active_scan_out)
+                    else:
+                        time.sleep(5)
+                        nmap_out_file = genFinishedScanFileName(sched)
+                        shutil.move(nmap_active_scan_out, '/opt/xml/' + nmap_out_file)
+                        nmapout = os.popen('python3 ' + cdir + '/cve.py webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename'] + '').readlines()
+                        print(nmapout)
+                    time.sleep(10)
+                else:
+                    print("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
+                nextsched = min(nextsched, nextrun)
+            print("[DEBUG] nextsched:" + str(nextsched - time.time()))
+
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Successfully closed poll '))
