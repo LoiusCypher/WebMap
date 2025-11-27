72574c11a7f87ffc633a60e148cc1c2c89c75447
Helmut <helmut@knirsch.de>
2025-11-27 09:25:45 +0100

    Start with notes to move to DB models

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.core.validators import RegexValidator
-from datetime import datetim
+from datetime import datetime
 import hashlib
 import os
 
