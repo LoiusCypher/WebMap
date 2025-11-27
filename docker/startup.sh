c265c1171c907d7d2742dbce89e6839b9f40706f
Helmut <helmut@knirsch.de>
2025-11-27 08:03:51 +0100

    Start with notes to move to DB models

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ls -al '/opt/nmapdashboard/db.sqlite3'
 sqlite3 '/opt/nmapdashboard/db.sqlite3' ".tables;"
-bash /opt/nmapdashboard/nmapreport/nmap/runcron.sh > /dev/null 2>&1 &
+bash /opt/nmapdashboard/nmapreport/nmap/runcron.sh & # > /dev/null 2>&1 &
 python3 /opt/nmapdashboard/manage.py runserver 0:8000
