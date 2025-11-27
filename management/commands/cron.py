302ea5e169a9187c75c2477ad9fbef173b294bdc
Helmut <helmut@knirsch.de>
2025-11-28 00:38:30 +0100

    Hunting model issue

--- management/commands/cron.py
+++ management/commands/cron.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-from nmapreport.models import ScanJob
+from nmapreport.models import Scan, ScanJob
+import nmapreport.nmap as nmap
 # import nmapreport.nse.cron as cron
 import os
 import re
@@ -10,44 +11,6 @@ import shutil
 import subprocess
 from datetime import datetime
 
-
-cdir = os.path.dirname(os.path.realpath(__file__))
-
-
-def nsePath():
-	return os.path.join(cdir, 'nse')
-
-
-def gethours(f):
-	return {
-		'1h': 3600,
-		'1d': 86400,
-		'1w': 604800,
-		'1m': 2592000
-	}[f]
-
-
-def genActiveScanFilePath(sched):
-	return '/tmp/' + str(sched['number']) + '_' + sched['params']['filename'] + '.active'
-
-
-def genFinishedScanFileName(sched):
-	return 'webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename']
-
-
-def genScanCmd(sched):
-	return [shutil.which('nmap')] + shlex.split(sched['params']['params']) + ['--script=' + nsePath() + '/',
-		'-oX', genActiveScanFilePath(sched), sched['params']['target']]
-
-
-def runScan(sched):
-	nmap_active_scan_out = genActiveScanFilePath(sched)
-	nmapprocess = subprocess.Popen(genScanCmd(sched), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-	stdout, stderr = nmapprocess.communicate()
-	print('[DONE] ' + stderr + stdout)
-	return nmapprocess.returncode, nmap_active_scan_out, stderr, stdout
-
-
 class Command(BaseCommand):
 
     def cronMe(self):
@@ -56,49 +19,36 @@ class Command(BaseCommand):
 
         self.stdout.write(str(ScanJob.objects.all().count()))
         for sched in ScanJob.objects.all():
-            sched.date_last_execution += datetime.now() + gethours(sched.execution_interval_numer)
             self.stdout.write(str(sched.date_last_execution))
-            sched.execution_counter += 1
-            self.stdout.write(str(sched.execution_counter))
-            sched.Save()
-        nextsched = time.time() + gethours('1m')
-        for i in schedfiles:
-            if re.search(r'^[a-f0-9]{32,32}\.json$', i.strip()) is not None:
-                try:
-                    sched = json.loads(open('/opt/schedule/' + i, "r").read())
-                except ValueError as e:
-                    print("Error in json content of ", i, ': ', e)
-                    continue
-                except IOError as e:
-                    print("Error while rading  ", i, ': ', e)
-                    continue
+            self.stdout.write(str(sched.execution_interval_numer))
+            nextrun = sched.date_last_execution + nmap.gethours(sched.execution_interval_numer)
+            print("[RUN]   scan:" + sched.name + " id:" + str(sched.id) + " (nextrun:" + str(nextrun) + " / now:" + str(datetime.now()) + ")")
+            if nextrun <= datetime.now():
+                sched.date_last_execution += datetime.now() + nmap.gethours(sched.execution_interval_numer)
+                sched.execution_counter += 1
+                self.stdout.write(str(sched.execution_counter))
+                sched.save()
+                scan = Scan(sched.name, options=sched.options, target=sched.target, execution_counter=sched.execution_counter)
+            else:
+                scan = Scan(sched.name, options=sched.options, target=sched.target)
+            scan.save()
+            nmap.runScan(scan)
 
-                nextrun = (sched['lastrun'] + gethours(sched['params']['frequency']))
-                if nextrun <= time.time():
-                    sched['number'] = (sched['number'] + 1)
-                    print("[RUN]   scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
-
-                    # first make sure to write down current run
-                    sched['lastrun'] = time.time()
-                    f = open('/opt/schedule/' + i, "w")
-                    f.write(json.dumps(sched, indent=4))
-                    nextrun = sched['lastrun'] + gethours(sched['params']['frequency'])
-
-                    errorCode, nmap_active_scan_out, stdout, stderr = runScan(sched)
-                    print('[DONE] ' + stderr + stdout)
-                    if errorCode != 0:
-                        os.remove(nmap_active_scan_out)
-                    else:
-                        time.sleep(5)
-                        nmap_out_file = genFinishedScanFileName(sched)
-                        shutil.move(nmap_active_scan_out, '/opt/xml/' + nmap_out_file)
-                        nmapout = os.popen('python3 ' + cdir + '/cve.py webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename'] + '').readlines()
-                        print(nmapout)
-                    time.sleep(10)
-                else:
-                    print("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
-                nextsched = min(nextsched, nextrun)
-            print("[DEBUG] nextsched:" + str(nextsched - time.time()))
+            #        # errorCode, nmap_active_scan_out, stdout, stderr = runScan(sched)
+            #        # print('[DONE] ' + stderr + stdout)
+            #        # if errorCode != 0:
+            #            # os.remove(nmap_active_scan_out)
+            #        # else:
+            #            # time.sleep(5)
+            #            # nmap_out_file = genFinishedScanFileName(sched)
+            #            # shutil.move(nmap_active_scan_out, '/opt/xml/' + nmap_out_file)
+            #            # nmapout = os.popen('python3 ' + cdir + '/cve.py webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename'] + '').readlines()
+            #            # print(nmapout)
+            #        # time.sleep(10)
+            #    # else:
+            #        # print("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
+            #    # nextsched = min(nextsched, nextrun)
+            # print("[DEBUG] nextsched:" + str(nextsched - time.time()))
 
 
     def handle(self, *args, **options):
