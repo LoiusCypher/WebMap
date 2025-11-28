214db6d7140f9382501c5a6e21c512925f02dada
Helmut <helmut@knirsch.de>
2025-11-28 04:13:37 +0100

    Fix typo

--- management/commands/cron.py
+++ management/commands/cron.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
             self.stdout.write(str(sched.execution_interval_numer))
             nextrun = sched.date_last_execution + nmap.gethours(sched.execution_interval_numer)
             print("[RUN]   scan:" + sched.name + " id:" + str(sched.id) + " (nextrun:" + str(nextrun) + " / now:" + str(datetime.now()) + ")")
-            timeToWait =  nextrun - datetime.now()
+            timeToWait = nextrun - datetime.now()
             if nextrun <= datetime.now():
                 sched.date_last_execution = datetime.now()
                 nextrun = sched.date_last_execution + nmap.gethours(sched.execution_interval_numer)
@@ -39,21 +39,21 @@ class Command(BaseCommand):
         print(timeToWait)
         return timeToWait
 
-            #        # errorCode, nmap_active_scan_out, stdout, stderr = runScan(sched)
-            #        # print('[DONE] ' + stderr + stdout)
-            #        # if errorCode != 0:
-            #            # os.remove(nmap_active_scan_out)
-            #        # else:
-            #            # time.sleep(5)
-            #            # nmap_out_file = genFinishedScanFileName(sched)
-            #            # shutil.move(nmap_active_scan_out, '/opt/xml/' + nmap_out_file)
-            #            # nmapout = os.popen('python3 ' + cdir + '/cve.py webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename'] + '').readlines()
-            #            # print(nmapout)
-            #        # time.sleep(10)
-            #    # else:
-            #        # print("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
-            #    # nextsched = min(nextsched, nextrun)
-            # print("[DEBUG] nextsched:" + str(nextsched - time.time()))
+        #        # errorCode, nmap_active_scan_out, stdout, stderr = runScan(sched)
+        #        # print('[DONE] ' + stderr + stdout)
+        #        # if errorCode != 0:
+        #            # os.remove(nmap_active_scan_out)
+        #        # else:
+        #            # time.sleep(5)
+        #            # nmap_out_file = genFinishedScanFileName(sched)
+        #            # shutil.move(nmap_active_scan_out, '/opt/xml/' + nmap_out_file)
+        #            # nmapout = os.popen('python3 ' + cdir + '/cve.py webmapsched_' + str(sched['lastrun']) + '_' + sched['params']['filename'] + '').readlines()
+        #            # print(nmapout)
+        #        # time.sleep(10)
+        #    # else:
+        #        # print("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(nextrun) + " / now:" + str(time.time()) + ")")
+        #    # nextsched = min(nextsched, nextrun)
+        # print("[DEBUG] nextsched:" + str(nextsched - time.time()))
 
 
     def handle(self, *args, **options):
