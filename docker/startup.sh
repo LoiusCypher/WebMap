9af62ffba22075ddd283bbc57925febcfcac1269
Helmut <helmut@knirsch.de>
2025-11-28 01:30:47 +0100

    Hunting model issue

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ls -al '/opt/nmapdashboard/db.sqlite3'
+python3 /opt/nmapdashboard/manage.py makemigrations
 python3 /opt/nmapdashboard/manage.py migrate --run-syncdb
 sqlite3 '/opt/nmapdashboard/db.sqlite3' ".tables '%';"
 sqlite3 '/opt/nmapdashboard/db.sqlite3' "SELECT * FROM nmapreport_scanjob;"
