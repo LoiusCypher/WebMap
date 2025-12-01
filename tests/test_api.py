c36d29e8ee027e000245414222176bbd0a1531f8
Helmut <helmut@knirsch.de>
2025-12-01 01:49:20 +0100

    Resolve lint issues

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,8 +10,8 @@ class ApiTestCase(TestCase):
                 'filename': 'testscan',
                 'params': '-sT A -T4',
                 'target': '192.168.1.1',
-                },
-            }
+            },
+        }
 
 	def test_api_nmap_newscan(self):
 		self.assertEqual(api.nmap_newScan(self.nmap_newscan_once), '/home/runner/work/WebMap/WebMap/nmapdashboard/nmapreport/nse')
