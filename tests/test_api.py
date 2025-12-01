31b0bb9401ad34fbc5b79b9e9c8c85b785ea274e
Helmut <helmut@knirsch.de>
2025-12-01 11:02:55 +0100

    Adjust test to changed sources

--- tests/test_api.py
+++ tests/test_api.py
@@ -24,8 +24,10 @@ class ApiTestCase(TestCase):
 
     def test_api_nmap_newscan_call(self):
         response = api.nmap_newscan(self.nmap_newscan_once)
+        response.raise_for_status()
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(json.dumps(response.json(), indent=4)), '/home/runner/work/WebMap/WebMap/nmapdashboard/nmapreport/nse')
+        self.assertEqual(response.msg, '/home/runner/work/WebMap/WebMap/nmapdashboard/nmapreport/nse')
+        # self.assertEqual(json.loads(json.dumps(response.json(), indent=4)), '/home/runner/work/WebMap/WebMap/nmapdashboard/nmapreport/nse')
 
 
 #    def test_api_nmap_newscan_post(self):
