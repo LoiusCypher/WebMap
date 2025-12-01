a8359e6040e2fac4e0d89ee744a604fb6eecf0d9
Helmut <helmut@knirsch.de>
2025-12-01 01:38:51 +0100

    Start api testing

---
+++ tests/test_api.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+import nmapreport.api as api
+
+
+class ApiTestCase(TestCase):
+	def setUp(self):
+		self.nmap_newscan_once = {
+			'request.method': 'POST',
+            'POST': {
+                'filename' : 'testscan',
+                'params' : '-sT A -T4',
+                'target' : '192.168.1.1',
+                },
+            }
+
+	def test_api_nmap_newscan(self):
+		self.assertEqual(api.nmap_newScan(self.nmap_newscan_once), '/home/runner/work/WebMap/WebMap/nmapdashboard/nmapreport/nse')
